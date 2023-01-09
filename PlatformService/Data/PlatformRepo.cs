8ccb3fba4922047a3beaa130e5cfec50e590c11c
kivanc <erturkkivanc@gmail.com>
2023-01-09 16:01:11 +0300

    WepApi Project Create

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+  public class PlatfromRepo : IPlatformRepo {
+    private readonly AppDbContext _context;
+    public PlatfromRepo(AppDbContext context) {
+      _context = context;
+    }
+    public void CreatePlatform(Platform plat) {
+      if (plat == null) {
+        throw new ArgumentNullException(nameof(plat));
+      }
+      _context.Platforms.Add(plat);
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms() {
+      return _context.Platforms.ToList();
+    }
+    public Platform GetPlatformById(int id) {
+      return _context.Platforms.FirstOrDefault(p => p.Id == id);
+    }
+    public bool SaveChanges() {
+      return (_context.SaveChanges() >= 0);
+    }
+    
+  }
+}

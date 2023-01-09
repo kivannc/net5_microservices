47737357bede2a3f18e83c6e65e4f95b881154c4
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-09 21:42:15 +0300

    Http Post Added

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -28,7 +29,7 @@ namespace PlatformService.Controllers
       return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetPlatformById")]
     public ActionResult GetPlatformById(int id)
     {
       var platformItem = _repository.GetPlatformById(id);
@@ -40,5 +41,18 @@ namespace PlatformService.Controllers
 
       return NotFound();
     }
+
+    [HttpPost]
+    public ActionResult CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+      var platformModel = _mapper.Map<Platform>(platformCreateDto);
+      _repository.CreatePlatform(platformModel);
+      _repository.SaveChanges();
+
+      var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+      return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+    }
+
   }
 }

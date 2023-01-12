653f35edcd94983f5a95261f71be66f875fe8992
kivanc <erturkkivanc@gmail.com>
2023-01-12 14:46:36 +0300

    PlatformService Migration

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -13,17 +13,31 @@ namespace PlatformService
 {
   public class Startup
   {
-    public Startup(IConfiguration configuration)
+    public IConfiguration Configuration { get; }
+
+    private readonly IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
       Configuration = configuration;
+      _env = env;
     }
-
-    public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<AppDbContext>(opt =>
-        opt.UseInMemoryDatabase("InMem"));
+      if (_env.IsProduction())
+      {
+        Console.WriteLine("--> Using SQL Server Db");
+        services.AddDbContext<AppDbContext>(opt =>
+          opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConnection")));
+      }
+      else
+      {
+        Console.WriteLine("--> Using In-Memory Db");
+        services.AddDbContext<AppDbContext>(opt =>
+          opt.UseInMemoryDatabase("InMem"));
+      }
+
       services.AddScoped<IPlatformRepo, PlatformRepo>();
 
       services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
@@ -63,7 +77,7 @@ namespace PlatformService
         endpoints.MapControllers();
       });
 
-      PrepDb.PrepPopulation(app);
+      PrepDb.PrepPopulation(app, env.IsProduction());
     }
   }
 }

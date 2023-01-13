a08ab694e02316d49f90282dada1a548a677d427
kivanc <erturkkivanc@gmail.com>
2023-01-13 16:34:13 +0300

    initrabbitmq

--- CommandsService/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -23,6 +23,8 @@ namespace CommandsService.AsyncDataServices
         {
             _configuration = configuration;
             _eventProcessor = eventProcessor;
+
+            InitializeRabbitMQ();
         }
 
         private void InitializeRabbitMQ()
